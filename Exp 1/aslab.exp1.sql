66eeb8f38b843fb1252df3c6aea415c32e5ce5d7
NAJEEB <44603860+najeebkm@users.noreply.github.com>
2020-11-06 17:04:22 +0530

    Add files via upload

---
+++ Exp 1/aslab.exp1.sql
@@ -0,0 +1,25 @@
+CREATE TABLE student (
+	sno INTEGER,
+    name CHAR(20),
+    marks INTEGER,
+    dept CHAR(5)
+);
+
+ALTER TABLE student
+ADD COLUMN age INTEGER;
+
+ALTER TABLE student
+MODIFY COLUMN dept CHAR(4);
+
+ALTER TABLE student
+DROP COLUMN marks;
+
+ALTER TABLE student
+RENAME TO students;
+
+INSERT INTO students
+VALUES(1,'NAJEEB','CSE',20);
+
+TRUNCATE students;
+
+DROP TABLE students;
