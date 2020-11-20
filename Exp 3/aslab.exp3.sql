07c6d045697c13646a2627a584de00299a113d90
NAJEEB <44603860+najeebkm@users.noreply.github.com>
2020-11-20 16:42:08 +0530

    Update aslab.exp3.sql

--- Exp 3/aslab.exp3.sql
+++ Exp 3/aslab.exp3.sql
@@ -1,18 +1,35 @@
-CREATE TABLE employeedetails(
-    empid CHAR(4),
-    Firstname  CHAR(10),
-    Lastname CHAR(10),
-    location CHAR(10),
-    sex CHAR(2)
-    );
+CREATE TABLE Employee 
+(
+    `empid` INT UNSIGNED PRIMARY KEY,
+    `first_name` VARCHAR(30) NOT NULL,
+    `last_name` VARCHAR(30) NOT NULL,
+    `salary` INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE EmployeeDetails 
+(
+    `empid` INT UNSIGNED,
+    `address` LONGTEXT NOT NULL,
+    `dob` DATE NOT NULL,
+    FOREIGN KEY(`empid`) REFERENCES Employee(`empid`)
+);
  
-    INSERT INTO employeedetails 
-    VALUES ('G1','Govind','sharma','Banglore','M'),
-    ('G2','Akshay','Krishna','New Delhi','M'),
-    ('G1','Muhammed','Afsal','Kerala','F');
+INSERT INTO Employee (`empid`, `first_name`, `last_name`, `salary`)
+VALUES (1, "PETER", "CHRIS", 12000),
+       (2, "MUHAMMED", "AFSAL", 2000),
+       (3, "GOVIND", "MEHTA", 100000);
+       
+INSERT INTO EmployeeDetails (`empid`, `address`, `dob`)
+VALUES (1, "NEW DELHI", "1993-2-22"),
+       (2, "KOLKATA", "1993-5-20"),
+       (3, "CHENNAI", "1998-4-12"); 
+
+
+
+SELECT * FROM Employee
+WHERE empid IN (SELECT empid FROM employeedetails);
     
-    SELECT * FROM Employee
-    WHERE empid IN (SELECT empid FROM employeedetails);
     
-     SELECT * FROM Employee
-    WHERE empid NOT IN (SELECT empid FROM employeedetails);
+
+SELECT * FROM Employee
+WHERE empid NOT IN (SELECT empid FROM employeedetails);
