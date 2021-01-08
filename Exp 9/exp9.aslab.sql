35161048e82babb90b98a0d9bc457bfe8f118245
NAJEEB <44603860+najeebkm@users.noreply.github.com>
2021-01-08 22:55:30 +0530

    Update exp9.aslab.sql

--- Exp 9/exp9.aslab.sql
+++ Exp 9/exp9.aslab.sql
@@ -9,10 +9,10 @@ CREATE TABLE Employee(
 
 INSERT INTO Employee(code, name, dob, designation, salary)
 VALUES
-('ar69','Aravind R','2000-02-02','CTO',190000.78),
-('ku12','Kiran U','2000-01-12','CEO',185000.78),
-('sv13','V Sam','1998-08-02','Clerk',59000.78),
-('ask1','A Siraj','1969-04-20','Auditor',59000.18);
+('ar69','Najeeb','2000-02-02','CTO',190000.78),
+('ku12','Jalal','2000-01-12','CEO',185000.78),
+('sv13','Jonathan','1998-08-02','Clerk',59000.78),
+('ask1','Siraj','1969-04-20','Auditor',59000.18);
 
 #2
 SELECT * FROM Employee ORDER BY name DESC;
